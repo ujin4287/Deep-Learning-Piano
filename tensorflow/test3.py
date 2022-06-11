e513918035aa4ace7af49fc97a27adad01e66502
ujin4287 <ujin4287@gmail.com>
2022-06-11 17:18:24 +0900

    Delete Temp file

--- tensorflow/test3.py
+++
@@ -1,7 +0,0 @@
-import re
-
-tmp_string = '11a22bb333'
-numbers = re.findall("\d+", tmp_string)
-print ('Exam For http://musclebear.tistory.com')
-print ('')
-print ('%s ==> %s'%(tmp_string,numbers))
